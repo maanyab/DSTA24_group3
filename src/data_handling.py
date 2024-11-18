228f55a42f18830d28ca917395296d1908c7520a
maanyab <maanyabagga2@gmail.com>
2024-11-18 23:00:30 +0100

    Revert "Replacing the old folder with the updated version from the feature branch"
    This reverts commit ba7290e4ba3dad31b89f8ee57d45878cd105a593.
    
    replacing the code files with updated files from the feature branch
    :wq

---
+++ src/data_handling.py
@@ -0,0 +1,36 @@
+"""
+Title: Data Handling
+Description: Functions to load and Preprocess MNIST Datasets
+"""
+
+
+import numpy as np
+import keras
+
+
+def prepare_data(num_classes=10):
+    """
+    Loads and Preprocess the MNIST dataset to prepare it for 
+    training and testing.
+    """
+    # Load the data and split it between train and test sets
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+    # Scale images to the [0, 1] range
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+
+    # Reshape images to include the channel dimension
+    x_train = np.expand_dims(x_train, -1)
+    x_test = np.expand_dims(x_test, -1)
+    
+    # Print dataset information
+    print("x_train shape:", x_train.shape)
+    print(x_train.shape[0], "train samples")
+    print(x_test.shape[0], "test samples")
+    
+    # Convert class vectors to binary class matrices (one-hot encoding)
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+    
+    return x_train, y_train, x_test, y_test
---
+++ src/neuralnet_architecture.py
@@ -0,0 +1,32 @@
+"""
+Title: Builing  a Neural Network Model
+Description: Function to build a neural network model 
+"""
+
+from keras import Sequential
+from keras import layers
+
+def neuralnet_model(input_shape, num_classes):
+    """
+    Create a Convolutional Neural Network (CNN) model.
+
+        input_shape (tuple): Shape of the input data.
+        num_classes (int): Number of output classes.
+
+    It returns a Sequential model
+    """
+    model = Sequential(
+        [
+            layers.Input(shape=input_shape),
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Flatten(),
+            layers.Dropout(0.5),
+            layers.Dense(num_classes, activation="softmax"),
+        ]
+    )
+    return model
+
+
