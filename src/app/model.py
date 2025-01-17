ebe52dea3b4912935d248909bcfd3967c423543f
maanyab <maanya.bagga@gmail.com>
2025-01-17 10:18:34 +0000

    Summary moved to a different folder model.py

---
+++ src/app/model.py
@@ -0,0 +1,14 @@
+import numpy as np
+import tensorflow as tf
+
+def load_model():
+	# path to the model inside the Docker Container
+	model_path = 'src/app/model/mnist_model.keras'
+	model = tf.keras.models.load_model(model_path)
+	return model
+
+def prediction(model, image_array):
+
+	# Get the model's prediction
+	prediction = model.predict(image_array)
+	return np.argmax(prediction)
