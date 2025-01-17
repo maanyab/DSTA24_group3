16d0478456bf3987e52996a2b95570099c331fd4
Maanya <154000585+maanyab@users.noreply.github.com>
2025-01-17 19:59:14 +0530

    Updated __init__.py

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,17 +1,3 @@
 from flask import Flask
-
-def create_app():
-
-	app = Flask(__name__)
-
-	# Initialise the database connection
-	from app.database import connect_db
-	connect_db()
-
-	# Register Blueprint
-	from app.views import main_bp
-	app.register_blueprint(main_bp)
-
-
-	return app
-
+app = Flask(__name__)
+from app import routes
