63b7ac4200071244ae8e1496acc3622259e50106
Maanya <154000585+maanyab@users.noreply.github.com>
2025-01-17 14:10:32 +0530

    Updated the views with minor changes

--- src/app/views.py
+++ src/app/views.py
@@ -3,7 +3,7 @@ import numpy as np
 import tensorflow as tf
 import io
 from flask import Blueprint, request, jsonify
-from model import load_model, prediction
+from app.model import load_model, prediction
 from app.database import save_image
 from PIL import Image
 
@@ -12,20 +12,19 @@ main_bp = Blueprint('main', __name__)
 #load pre trained keras model
 model =  load_model()
 
-@main_bp.route('/predict', methods=['POST'])
-
+@main_bp.route('/predict',methods=['POST'])
 def predict():
 	try:
-		#Get the JSON payload from the POST request
+#Get the JSON payload from the POST request
 		data =  request.get_json()
 
 		if 'image' not in data:
-		    return jsonify({'error': 'No image provided in the request'}), 400
+			return jsonify({'error': 'No image provided in the request'}), 400
 
 		# Decode the base64 image to a numpy array
 		image_data = base64.b64decode(data['image'].split(',')[-1])
 		image = Image.open(io.BytesIO(image_data)).convert('L')
-		image=image.resize((28,28))
+		image =image.resize((28,28))
 		image_array = np.array(image)
 
 		# Reshaping the image for neural network
@@ -33,10 +32,10 @@ def predict():
 
 		#Get Predictions
 		predictions = prediction(model, image_array)
-		predicted_label=int(np.argmax(predictions))
+		predicted_label = int(np.argmax(predictions))
 
 		#Save preditions to the Database
-		save_image(predictions,image_array)
+		save_image(predictions, image_array)
 
 		#Return the prediction to the user
 		return jsonify({'prediction': int(predicted_label)})
